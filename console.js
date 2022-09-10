8432a021ea6c60d71d44ad9322d92a78dfe5d8e4
lynn <106866928+lynne-p@users.noreply.github.com>
2022-09-10 11:05:27 +0800

    添加了html，js，css文件
    主要是试试上传之后能否访问

---
+++ console.js
@@ -0,0 +1,8 @@
+console.log(`%c %c\n如果恋爱有颜色，那一定是纯白色！\n恋に色があるとすれば、きっと純白です。\n所属社团/组织：Dream-Mad-team（大馒头映画）:https://space.bilibili.com/290451282/\n `,
+  ` padding: 35px 200px;
+    background-image: url(https://api.beixibaobao.com/img/blog/logo.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    color: transparent;`, 
+  'color: #black; font-size: 16px;margin-bottom: 10px;',
+);
